e8f75af3332e30358552caec7e32a950221f3ea5
slyzOwO <lilithhimes@gmail.com>
2025-06-06 20:18:38 -0500

    Update 1_mathFunctions.cpp

--- 2_Library Functions/1_mathFunctions.cpp
+++ 2_Library Functions/1_mathFunctions.cpp
@@ -3,7 +3,35 @@
 
 int main(){
 
+  double x = 3;
+  double y = 4.121;
+  double z;
 
+// in the <iostream>
+  z = std::max(x, y);
+  std::cout << z << '\n';
+
+  z = std::min(x, y);
+  std::cout << z << '\n';
+
+// in the <cmath>
+  z = pow(2, 3);
+  std::cout << z << '\n';
+
+  z = sqrt(2);
+  std::cout << z << '\n';
+
+  z = abs(-10);
+  std::cout << z << '\n';
+
+  z = round(y);
+  std::cout << z << '\n';
+
+  z = ceil(y);
+  std::cout << z << '\n';
+
+  z = floor(y);
+  std::cout << z << '\n';
 
 return 0;
 }
