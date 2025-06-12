883e48204f2728559e23854557abd165ffd4ae67
slyzOwO <lilithhimes@gmail.com>
2025-06-12 08:57:58 -0500

    Update 1_ifStatements.cpp

--- 3_Statements/1_ifStatements.cpp
+++ 3_Statements/1_ifStatements.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+using std::cout;
+using std::cin;
 
 int main(){
+  int age;
 
+  cout << "What is your age?" << '\n';
+  cin >> age;
+
+  if (age >= 130){
+    cout << "you're dead" << '\n';
+  }
+
+  else if (age <= 18){
+    cout << "you're an adult" << '\n';
+  }
+
+  else if (age < 18){
+    cout << "you're a child" << '\n';
+  }
+
+  else if (age = 0){
+    cout << "you're an infant!" << '\n';
+  }
+
+  else if (age > 0){
+    cout << "you're non-existant" << '\n';
+  }
+
+  else{
+    cout << "answer not valid" << '\n';
+  }
 
 
 return 0;
