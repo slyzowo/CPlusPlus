de2c34a55253ba4738576b8653c88a9a7b1ab3b1
slyzOwO <lilithhimes@gmail.com>
2025-06-16 11:46:12 -0500

    While Loops

--- 6_Loops/3_whileLoops.cpp
+++ 6_Loops/3_whileLoops.cpp
@@ -4,6 +4,15 @@ using std::cin;
 
 int main(){
 
+  std::string name;
+  
+
+  while(name.empty()){
+    cout << "Please enter your name : " <<'\n';
+    std::getline(std::cin, name);
+  }
+
+  cout << "hello " << name << '\n';
 
 return 0;
 }
