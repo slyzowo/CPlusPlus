31ba638d13972ad3770e7e239a188a1893d1400a
slyzOwO <lilithhimes@gmail.com>
2025-06-16 10:02:17 -0500

    String Functions

--- 2_Library Functions/2_stringFunctions.cpp
+++ 2_Library Functions/2_stringFunctions.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+using std::cout;
+using std::string;
 
 int main(){
 
+  string name;
 
+  cout << "what is your name?" << '\n';
+  std::getline(std::cin, name);
+
+  if(name.length() > 12){
+    cout << "your name cannot be over 12 characters" << '\n';
+  }
+
+  else if (name.empty()){
+    cout << "you did not enter your name" << '\n';
+  }
+  
+  else{
+    cout << "your new gmail is " << name.append("@gmail.com") << '\n';
+    cout << "I N S E R T I N G" << '\n';
+    cout << name.insert(6, "inserted") << '\n';
+    cout << name.find("@") << '\n';
+    cout << name.erase(1) << '\n';
+  }
 
 return 0;
 }
